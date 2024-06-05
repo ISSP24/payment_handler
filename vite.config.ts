28d97c8625ac2d20e915dabe93cf11390da2dbf0
vivek_2401 <vivek.m@iqm.com>
2024-06-05 09:08:40 +0530

    Published

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
     plugins: [react()],
     build: {
-        rollupOptions: {},
+        rollupOptions: {
+            output: {
+                dir: 'docs',
+            },
+        },
     },
 });
