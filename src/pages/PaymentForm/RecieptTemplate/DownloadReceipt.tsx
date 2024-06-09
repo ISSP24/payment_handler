5951e1fc2f602a587a59671b68f28163b6ea985b
vivek_2401 <vivek.m@iqm.com>
2024-06-09 21:55:59 +0530

    Added field

--- src/pages/PaymentForm/RecieptTemplate/DownloadReceipt.tsx
+++ src/pages/PaymentForm/RecieptTemplate/DownloadReceipt.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { toJpeg } from 'html-to-image';
+import saveAs from 'file-saver';
 
 export const downloadReceipt = (
     elementRef: React.RefObject<HTMLElement>,
@@ -7,12 +8,21 @@ export const downloadReceipt = (
 ) => {
     if (elementRef.current) {
         toJpeg(elementRef.current, { cacheBust: false })
-            .then((dataUrl) => {
-                const link = document.createElement('a');
-                link.download = name + '.jpeg';
-                link.href = dataUrl;
-                console.log({ dataUrl });
-                link.click();
+            .then((dataUrl: string) => {
+                const base64String = dataUrl.replace(
+                    'data:image/jpeg;base64,',
+                    ''
+                );
+
+                var byteCharacters = atob(base64String);
+                var byteNumbers = new Array(byteCharacters.length);
+                for (var i = 0; i < byteCharacters.length; i++) {
+                    byteNumbers[i] = byteCharacters.charCodeAt(i);
+                }
+                var byteArray = new Uint8Array(byteNumbers);
+                var blob = new Blob([byteArray], { type: 'image/png' }); // Specify the appropriate MIME type
+
+                saveAs(blob, name + '.jpeg');
             })
             .catch((err) => {
                 console.log(err);
